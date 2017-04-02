80fde0cd81f2b3b08b809d969d5bda9ca00cc456
Aliot <aliot@tut.by>
2017-04-02 20:30:19 +0300

    Add some changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import {Component} from '@angular/core';
 import {environment} from "./constants/environment";
-import {User} from "./model/user";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    public currentUser: User;
-    logout(): void {
-        this.currentUser = null;
-        localStorage.removeItem(environment.USER_CONST);
 
+    logout(): void {
+        localStorage.removeItem(environment.USER_CONST);
     }
 }
